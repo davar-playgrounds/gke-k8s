e8d444d19c3156b73f37d64a22af9187984fd2ad
Michael Haddon <dis_connected@ymail.com>
2018-12-16 01:51:54 +0100

    added db k8s yamls

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gcloud container clusters create mhaddon-k8-test --cluster-version="1.11.3-gke.18" --num-nodes="3" --addons="KubernetesDashboard"
+gcloud container clusters create mhaddon-k8-test --cluster-version="1.11.3-gke.18" --num-nodes="3" --addons="KubernetesDashboard" --enable-network-policy --no-enable-legacy-authorization
