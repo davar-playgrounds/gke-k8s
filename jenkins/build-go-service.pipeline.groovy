68ee628e2411b546b8eedb41ee8beb4295a58892
Michael Haddon <dis_connected@ymail.com>
2018-12-17 23:23:04 +0100

    removed old registry code

--- jenkins/build-go-service.pipeline.groovy
+++ jenkins/build-go-service.pipeline.groovy
@@ -2,10 +2,24 @@ pipeline {
   agent any
 
   stages {
+    stage('Login To Docker') {
+      steps {
+        sh "set +x; docker login -u '${env.GCLOUD_USER_NAME.trim()}' -p '${env.GCLOUD_ACCESS_TOKEN.trim()}' https://${env.GCLOUD_HOSTNAME.trim()}"
+      }
+    }
+
     stage('Build & Compile') {
       steps {
         dir("./services/${env.SERVICE_NAME}") {
-          sh "./build.sh ${env.IMAGE_TAG}"
+          sh "docker build -t ${env.GCLOUD_HOSTNAME.trim()}/${env.GCLOUD_PROJECTNAME.trim()}/${env.SERVICE_NAME}:${env.IMAGE_TAG} ."
+        }
+      }
+    }
+
+    stage('Push') {
+      steps {
+        dir("./services/${env.SERVICE_NAME}") {
+          sh "docker push ${env.GCLOUD_HOSTNAME.trim()}/${env.GCLOUD_PROJECTNAME.trim()}/${env.SERVICE_NAME}:${env.IMAGE_TAG}"
         }
       }
     }
