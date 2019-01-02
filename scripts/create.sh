04f58a44f5417d86a8f763188946a5649a8744da
Michael Haddon <dis_connected@ymail.com>
2019-01-02 16:55:30 +0100

    added more learning

--- scripts/create.sh
+++ scripts/create.sh
@@ -2,9 +2,11 @@
 
 gcloud container clusters create "${CLUSTER_NAME}" \
  --cluster-version="${CLUSTER_VERSION}" \
- --num-nodes="${CLUSTER_NODES}" \
+ --num-nodes="${CLUSTER_NODES_PER_ZONE}" \
  --machine-type="${CLUSTER_MACHINE_TYPE}" \
  --addons="KubernetesDashboard" \
+ --region="${CLUSTER_REGION}" \
+ --node-locations="${CLUSTER_NODE_ZONES}" \
  --enable-network-policy \
  --no-enable-legacy-authorization \
  --scopes storage-rw
